88228d01095024dd14ec0f8ca21de8eab60970bf
Vornario <volodarskiy04@gmail.com>
2025-03-12 12:45:44 +1000

    parapara

--- user_class.py
+++ user_class.py
@@ -1,10 +1,10 @@
 class User:
-    def __init__(self, user_id, name, email, password, accounts):
+    def __init__(self, user_id, name, email, password):
         self.user_id = user_id
         self.name = name
         self.email = email
         self.password = password
-        self.accounts = accounts
+        self.accounts = []
 
     def add_account(self, account):
         self.accounts.append(account)
